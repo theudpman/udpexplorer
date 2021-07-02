86b938d054b84bd8f5a6d3876bf7d7bd5404f015
elias <yemista@gmail.com>
2021-07-02 15:38:43 -0400

    changed api url

--- udpexplorer/config.js
+++ udpexplorer/config.js
@@ -1,5 +1,5 @@
-//var api = 'http://45.33.77.14:11101';
-var api = 'http://127.0.0.1:11101';
+var api = 'http://45.33.77.14:11101';
+//var api = 'http://127.0.0.1:11101';
 
 var apiList = ["http://45.33.77.14:11101"];
 
@@ -11,7 +11,7 @@ var refreshDelay = 30000;
 var blocksPerPage = 20;
 var whiteTheme = "css/light.css";
 var nightTheme = "css/dark.css";
-var addressPattern = new RegExp("^K[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{94}$");
+var addressPattern = new RegExp("^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{94}$");
 // pools stats by MainCoins
 var poolsStat = 
 	[
